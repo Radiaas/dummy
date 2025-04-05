d240efb60fdd521293ce53db5db910821b0aa1cb
Radiaas <radiasadna@gmail.com>
2025-04-05 23:10:03 +0700

    update

--- app/src/main/java/com/colab/myfriend/activity/WorkManagerActivity.kt
+++ app/src/main/java/com/colab/myfriend/activity/WorkManagerActivity.kt
@@ -10,6 +10,7 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.provider.Settings
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
@@ -22,9 +23,11 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkRequest
+import com.colab.myfriend.InAppNotificationWorker
 import com.colab.myfriend.worker.NotificationWorker
 import com.crocodic.core.base.activity.NoViewModelActivity
 import com.crocodic.core.extension.snacked
+import com.crocodic.core.extension.text
 import com.crocodic.core.extension.tos
 import com.example.myfriend.R
 import com.example.myfriend.databinding.ActivityWorkManagerBinding
@@ -55,7 +58,35 @@ class WorkManagerActivity :
         binding.btnRequest.setOnClickListener {
             createWorkNotification()
         }
+
+        binding.btnInAppNotification.setOnClickListener {
+            createInAppNotification()
+        }
     }
+
+    private fun createInAppNotification() {
+        val inputSecond = binding.etCount.text.toString().toLong()
+
+        val notificationWorkRequest: WorkRequest =
+            OneTimeWorkRequestBuilder<InAppNotificationWorker>()
+                .setInitialDelay(inputSecond, TimeUnit.SECONDS)
+                .build()
+
+        WorkManager.getInstance(this).enqueue(notificationWorkRequest)
+
+        showCountDown(inputSecond)
+    }
+
+    private fun showCountDown(countdown: Long) {
+        object : CountDownTimer(TimeUnit.SECONDS.toMillis(countdown), 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                binding.tvCountdown.text("${millisUntilFinished / 1000}")
+            }
+
+            override fun onFinish() {}
+        }.start()
+    }
+
 
     private fun createWorkNotification() {
         val notificationWorkRequest: WorkRequest = OneTimeWorkRequestBuilder<NotificationWorker>()
